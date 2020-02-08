c5c92539acf35ecdaa945f8077eb49db16300fa4
MacheteZ5 <machetemontenegro@hotmail.com>
2020-02-07 22:48:30 -0600

    Se realizó la lectura del archivo y la creación del árbol

---
+++ Proyecto_1229918_Montenegro/Proyecto_1229918_Montenegro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Proyecto_1229918_Montenegro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lectura de archivos en bytes 
+            var File = string.Empty;
+            File = Console.ReadLine();
+            using (var stream = new FileStream(File, FileMode.Open))
+            {
+                var bufferLengt = 1000000;
+                using (var reader = new BinaryReader(stream))
+                {
+                    var byteBuffer = new byte[bufferLengt];
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        byteBuffer = reader.ReadBytes(bufferLengt);
+                        foreach (byte bit in byteBuffer)
+                        {
+
+                        }
+                    }
+                }
+            }
+            //Ingresar Al árbol
+            var tree = new Tree();
+            var element = new Elements();
+            element.caracter = '5';
+            var NNode = new Node(element);
+            tree.Ingresar(NNode);
+            tree.Ingresar(NNode);
+            tree.Ingresar(NNode);
+            tree.Ingresar(NNode);
+        }
+    }
+}
---
+++ Proyecto_1229918_Montenegro/Proyecto_1229918_Montenegro/Tree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_1229918_Montenegro
+{
+    public struct Elements
+    {
+        public char caracter;
+    }
+    public class Node        
+    {
+        public Elements elemento;
+        public Node hijoIZ;
+        public Node hijoDR;
+        public Node(Elements elementos)
+        {
+            elemento = elementos;
+            hijoIZ = null;
+            hijoDR = null;
+        }
+    }
+    public class Tree
+    {
+        public Node Raiz;
+        
+        public void Ingresar(Node valor)
+        {
+            if(Raiz == null)
+            {
+                Elements elemento = new Elements();
+                elemento.caracter = '.';
+                Node NRaiz = new Node(elemento);
+                NRaiz.hijoIZ = valor;
+                Raiz = NRaiz;
+            }
+            else
+            {
+                IngresarAux(valor);
+            }
+        }
+        void IngresarAux(Node valor)
+        {
+            Node VAux = new Node(valor.elemento);
+            if(Raiz.hijoDR==null)
+            {
+                Raiz.hijoDR = VAux;
+            }
+            else
+            {
+                Elements elemento = new Elements();
+                elemento.caracter = '.';
+                Node NRaiz = new Node(elemento);
+                NRaiz.hijoIZ = Raiz;
+                NRaiz.hijoDR = VAux;
+                Raiz = NRaiz;
+            }
+        }
+    }
+
+
+}
